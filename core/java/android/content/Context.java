be20e0b01c4e0a622de5a7fa16fb00cfc03bb491
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-08-10 22:06:22 -0300

    base: Port extended screenshot function from OOS
    Change-Id: I856597056cb56d479a7a2c47dfdfc199da4302b8
    Signed-off-by: Sujit Roy <kumarsujitroy@gmail.com>
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- core/java/android/content/Context.java
+++ core/java/android/content/Context.java
@@ -4696,6 +4696,12 @@ public abstract class Context {
     public static final String APPLOCK_SERVICE = "applock";
 
     /**
+     * Long screenshot
+     * @hide
+     */
+    public static final String LONGSCREENSHOT_SERVICE = "longshot";
+
+    /**
      * Determine whether the given permission is allowed for a particular
      * process and user ID running in the system.
      *
