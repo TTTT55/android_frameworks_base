a505546171dddfcd3ee0c2ed1400f9533163d686
TTTT55 <iamtttt55@gmail.com>
2021-01-18 04:28:51 +0000

    Fix build errors

--- packages/SystemUI/src/com/android/systemui/biometrics/BiometricDialogView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/BiometricDialogView.java
@@ -24,6 +24,7 @@ import static android.view.Gravity.CENTER;
 import static android.view.Gravity.START;
 
 import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.graphics.PixelFormat;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
@@ -91,6 +92,7 @@ public abstract class BiometricDialogView extends LinearLayout {
     private final int mErrorColor;
     private final float mDialogWidth;
     protected final DialogViewCallback mCallback;
+    private final PackageManager mPackageManager;
 
     protected final ViewGroup mLayout;
     protected final LinearLayout mDialog;
@@ -168,6 +170,7 @@ public abstract class BiometricDialogView extends LinearLayout {
         mWindowManager = mContext.getSystemService(WindowManager.class);
         mUserManager = mContext.getSystemService(UserManager.class);
         mDevicePolicyManager = mContext.getSystemService(DevicePolicyManager.class);
+        mPackageManager = mContext.getPackageManager();
         mAnimationTranslationOffset = getResources()
                 .getDimension(R.dimen.biometric_dialog_animation_translation_offset);
         mErrorColor = getResources().getColor(R.color.biometric_dialog_error);
@@ -370,7 +373,7 @@ public abstract class BiometricDialogView extends LinearLayout {
             if (mAppLockDialog) {
                 final CharSequence negText = mBundle.getCharSequence(BiometricPrompt.KEY_NEGATIVE_TEXT);
                 descriptionText += getResources().getString(R.string.applock_locked) + "\n"
-                        + negText + getResources().getString(getDescriptionTextId());
+                        + negText + getResources().getString(R.string.applock_fingerprint);
             }
             mDescriptionText.setText(descriptionText);
         }
